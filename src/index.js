d030f20c1dea25bb9ebc8bc29cd44fd71faa0f0f
Richard Valentim <richardvalentim.corp@gmail.com>
2025-01-20 01:50:41 -0300

    Projeto Finalizado!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react'; // Importa o useEffect
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const Root = () => {
+    useEffect(() => {
+
+        const preventZoom = (e) => {
+            if (e.ctrlKey || e.metaKey) {
+                e.preventDefault();
+            }
+        };
+
+
+        window.addEventListener('wheel', preventZoom, { passive: false });
+        window.addEventListener('keydown', preventZoom);
+
+
+        return () => {
+            window.removeEventListener('wheel', preventZoom);
+            window.removeEventListener('keydown', preventZoom);
+        };
+    }, []);
+
+    return (
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Root />);
+
 reportWebVitals();
